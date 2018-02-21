2d50569be12fbb0d71d2bb7b003d73d2fd95abb6
Sandeep Shetty <sandeep.shetty@gmail.com>
2018-02-21 16:43:56 +0530

    Create asana-board-toggle-column-visibility.js

---
+++ asana-board-toggle-column-visibility.js
@@ -0,0 +1,11 @@
+javascript:(function(){
+	var columnNumber = prompt("Please enter column you want to hide/unhide", "0");
+	var columnContainer = document.querySelector(".SortableList-itemContainer--row");
+	var columnElement = ColumnContainer.children[columnNumber];
+	if (columnElement.style.display === "none") {
+		columnElement.style.display = "block";
+	}
+	else {
+		columnElement.style.display = "none";
+	}
+})();
