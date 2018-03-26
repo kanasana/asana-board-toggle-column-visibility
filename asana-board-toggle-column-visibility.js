a5c380fbd4dc4aa32b4025fe27764b83938d71ea
Sandeep Shetty <sandeep.shetty@gmail.com>
2018-03-26 14:30:21 +0530

    Fixed typo ColumnContainer -> columnContainer

--- asana-board-toggle-column-visibility.js
+++ asana-board-toggle-column-visibility.js
@@ -1,7 +1,7 @@
 (function(){
 	var columnNumber = prompt("Please enter column you want to hide/unhide", "0");
 	var columnContainer = document.querySelector(".SortableList-itemContainer--row");
-	var columnElement = ColumnContainer.children[columnNumber];
+	var columnElement = columnContainer.children[columnNumber];
 	if (columnElement.style.display === "none") {
 		columnElement.style.display = "block";
 	}
